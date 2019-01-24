6c749bf3fd75ec657f6e5c116a855a03c9cf5695
Kevin Wen <legend0322@vip.qq.com>
2019-01-24 09:43:04 +0800

    修复闪退的问题

--- ZhibaoZone/AppDelegate.swift
+++ ZhibaoZone/AppDelegate.swift
@@ -310,7 +310,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         let value = UserDefaults.standard.value(forKey: "orignalOrientation")
-        UIDevice.current.setValue(value, forKey: "orientation")
+        if value == nil{
+            UIDevice.current.setValue(0, forKey: "orientation")
+        }else{
+            UIDevice.current.setValue(value, forKey: "orientation")
+        }
+        
       //  print("restored status \(value)")
     }
 
